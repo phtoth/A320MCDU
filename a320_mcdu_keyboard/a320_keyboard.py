4987e2acd4b7ead773946f51502216cd1780b3f4
Pedro Toth <pedrototh@gmail.com>
2018-09-21 19:01:19 +0000

    All Buttons Working

--- a320_mcdu_keyboard/a320_keyboard.py
+++ a320_mcdu_keyboard/a320_keyboard.py
@@ -7,6 +7,16 @@ GPIO.setwarnings(False)
 LINHA = [2,3,4,17,27,22,10,9,11]
 COLUNA = [14,15,18,23,24,25,8,12,16]
 
+COMANDOS = [['F1','F2','F3','F4','F5','F6','F7','F15','F16'],
+            ['F8','F9','F10','F11','F12','F13','F14','LEFT','UP'],
+            ['A','B','C','D','E','RIGHT','DOWN','1','2'],
+            ['3','F','G','H','I','J','4','5','6'],
+            ['7','8','9','DOT','0','PLUS','K','L','M'],
+            ['N','O','P','Q','R','S','T','U','V'],
+            ['W','X','Y','SL1','SL2','SL3','SL4','SL5','SL6'],
+            ['Z','SLASH','SP','SR1','SR2','SR3','SR4','SR5','SR6'],
+            ['OVFY','CLR']]
+
 for p in range(9):
     GPIO.setup(COLUNA[p],GPIO.OUT)
     GPIO.output(COLUNA[p], 1)
@@ -18,11 +28,10 @@ try:
     while(True):
         for m in range(9):
             GPIO.output(COLUNA[m],0)
-            
+
             for g in range(9):
                 if GPIO.input(LINHA[g]) == 0:
-                    print "Foi"
-                    print  LINHA[g], COLUNA[m]
+                    print  COMANDOS[g][m]
                     while (GPIO.input(LINHA[g]) == 0):
                         pass
                     sleep(0.3)
